5cae62efd989d2a06d97fb409c12e1957bfa6aac
Anton Sander <info@sanderinvest.com>
2017-03-08 19:10:06 +0100

    tenplate hinzugefügt 2

--- adm/admin.py
+++ adm/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
 from .models import currency
 from .models import broker
-from .models import brokeraccount
+from .models import broker_account
+from .models import invest_date
+from .models import entry_code
+from .models import user_account
+from .models import invest_plan
+from .models import user_profile
+
+class BrokerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+class BrokerAccountAdmin(admin.ModelAdmin):
+    list_display = ('broker','number','server','type','currency')
+    list_filter = ['broker']
+class InvestDateAdmin(admin.ModelAdmin):
+    list_display = ('week','year')
+    list_filter = ['week','year']
+class InvestPlanAdmin(admin.ModelAdmin):
+    list_display = ('invest_date','broker_account','balance_in','balance_out','open_pl','close_pl','dd')
+    list_filter = ['invest_date','broker_account']
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('id','invest_date','user','code','text','amount','balance')
+    list_filter = ['user','invest_date','code']
+
+class EntryCodeAdmin(admin.ModelAdmin):
+    list_display = ('id','text')
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','referral','einlage','auszahlung','profit','balance','deposit','currency','invest_date','pay_date','days')
 
 admin.site.register(currency)
-admin.site.register(broker)
-admin.site.register(brokeraccount)
+admin.site.register(broker, BrokerAdmin)
+admin.site.register(broker_account, BrokerAccountAdmin)
+admin.site.register(invest_date, InvestDateAdmin)
+admin.site.register(entry_code, EntryCodeAdmin)
+admin.site.register(user_account, UserAccountAdmin)
+admin.site.register(invest_plan, InvestPlanAdmin)
+admin.site.register(user_profile, UserProfileAdmin)
--- adm/models.py
+++ adm/models.py
@@ -12,7 +12,7 @@ class broker(models.Model):
 	def __str__(self):
 		return self.name
 
-class brokeraccount(models.Model):
+class broker_account(models.Model):
 	broker    = models.ForeignKey('adm.broker')
 	number    = models.CharField(max_length=32)
 	password  = models.CharField(max_length=255)
@@ -21,4 +21,52 @@ class brokeraccount(models.Model):
 	currency  = models.ForeignKey('adm.currency')
 	def __str__(self):
 		return self.number
-		
+
+class invest_date(models.Model):
+	year = models.IntegerField()
+	week = models.IntegerField()
+	def __str__(self):
+		return "%0.2d/%4d" % (self.week,self.year)
+
+class entry_code(models.Model):
+  text = models.CharField(max_length=32)
+  def __str__(self):
+    return self.text
+
+
+class user_account(models.Model):
+  user = models.ForeignKey('auth.user')
+  code = models.ForeignKey('adm.entry_code')
+  amount = models.DecimalField(max_digits=20,decimal_places=4)
+  invest_date = models.ForeignKey('adm.invest_date')
+  text = models.CharField(max_length=255,blank=True)
+  balance = models.DecimalField(max_digits=20,decimal_places=4,default=0)
+  def __str__(self):
+    return str(self.id)
+
+
+class invest_plan(models.Model):
+  invest_date = models.ForeignKey('adm.invest_date')
+  broker_account = models.ForeignKey('adm.broker_account')
+  balance_in = models.DecimalField(max_digits=20,decimal_places=4)
+  balance_out = models.DecimalField(max_digits=20,decimal_places=4)
+  open_pl = models.DecimalField(max_digits=20,decimal_places=4)
+  close_pl = models.DecimalField(max_digits=10,decimal_places=4,default=0)
+  dd = models.DecimalField(max_digits=10,decimal_places=4,default=0)
+  def __str__(self):
+    return str(self.invest_date)
+
+class user_profile(models.Model):
+  user = models.OneToOneField('auth.user')
+  referral = models.ForeignKey('auth.user', related_name='referral',default=1)
+  einlage = models.DecimalField(max_digits=20,decimal_places=2,default=0)
+  auszahlung = models.DecimalField(max_digits=20,decimal_places=2,default=0)
+  profit = models.DecimalField(max_digits=4,decimal_places=1,default=0.5)
+  balance = models.DecimalField(max_digits=20,decimal_places=2,default=0)
+  deposit = models.DecimalField(max_digits=20,decimal_places=2,default=0)
+  currency = models.ForeignKey('adm.currency')
+  invest_date = models.DateField(blank=True)
+  pay_date = models.DateField(blank=True)
+  days = models.IntegerField()
+  def __unicode__(self):
+    return self.user.username
